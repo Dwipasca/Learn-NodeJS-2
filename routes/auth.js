adda1b7b7c74988cfc2d73d3e96330b0b8afe6af
Dwi Pasca <dpnursanti@gmail.com>
2020-10-28 22:03:49 +0700

    add package bcrypt for encrypt the password

--- routes/auth.js
+++ routes/auth.js
@@ -1,17 +1,20 @@
 const router = require('express').Router()
 const User = require('../model/User')
+const bcrypt = require('bcryptjs')
 const userRegisterValidation  = require('../validation')
 
 
 // use function uservalidation first to validate data if success then the code bellow will be execute
 router.post('/register', userRegisterValidation, async (req, res) => {
 
-    
+    // encrypt the password
+    const salt = await bcrypt.genSalt(10)
+    const encryPassword = await bcrypt.hash(req.body.password, salt)
 
     const user = new User({
         name: req.body.name,
         email: req.body.email,
-        password: req.body.password
+        password: encryPassword
     })
 
     try {
